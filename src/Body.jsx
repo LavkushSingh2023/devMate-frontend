5b75f6fe3b68f886c5896e1f5d8291c9061037b7
Lavkush Singh <t21425814@gmail.com>
2025-01-18 17:59:13 +0530

    get data from after login

---
+++ src/Body.jsx
@@ -0,0 +1,42 @@
+import { Outlet, useNavigate } from "react-router-dom"
+import NavBar from "./NavBar"
+import Footer from "./Footer"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
+import axios from "axios"
+import { BASE_URL } from "./utils/constants"
+import {addUser} from "./utils/userSlice"
+
+const Body = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const userData = useSelector((store) => store.user)
+
+    const  fetchUser = async () => {
+        if(userData) return
+        
+        try{
+            const res = await axios.get(BASE_URL + "/profile/view", {
+                withCredentials: true
+            })
+            
+            dispatch(addUser(res.data))
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, [])
+
+    return(
+        <>
+            <NavBar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Body
---
+++ src/Login.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "./utils/constants";
+
+const Login = () => {
+    const [emailId, setEmailId] = useState("t21425814@gmail.com")
+    const [password, setPassword] = useState("LsT@00000")
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleLogin = async () => {
+        try{
+            const res = await axios.post(BASE_URL + "/login", {
+                emailId,
+                password
+            }, {withCredentials: true})
+            dispatch(addUser(res.data))
+            return navigate("/")
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    return (
+        <>
+            <div className="flex justify-center">
+                <div className="card bg-base-100 image-full w-96 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="card-title flex justify-center">LogIn</h2>
+                        <div>
+                            <label htmlFor="emailId">EmailId</label>
+                            <br />
+                            <input
+                                type="text"
+                                id="emailId"
+                                className="m-2 p-2 rounded"
+                                value={emailId} 
+                                onChange={(e) => setEmailId(e.target.value)}
+                            />
+                            <br />
+                            <label htmlFor="password">Password</label>
+                            <br />
+                            <input
+                                type="password"
+                                id="password"
+                                className="m-2 p-2 rounded"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="card-actions justify-center">
+                            <button className="btn btn-primary" onClick={handleLogin}>
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
